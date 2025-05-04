8532e31ead78caf689707cf3ceb53e89a435e0cd
llmgcc <llmgcc8926@gmail.com>
2025-05-04 12:46:55 +0300

    fix: update static

--- backend/src/general/schemas.py
+++ backend/src/general/schemas.py
@@ -1,15 +1,10 @@
 from sqlmodel import SQLModel
-# from typing import List, Any
-# from src.keyskills.schemas import ChartResponse
-# import datetime
-
 
 class CurrencyResponse(SQLModel):
     currency_code: str
     currency_abbr: str
     currency_name: str
     currency_rate: float
-
 
 class StatsResponse(SQLModel):
     last_update: str
