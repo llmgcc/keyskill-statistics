3a6e519fc97dfbb184c85ea85d92e02d22ace117
llmgcc <llmgcc8926@gmail.com>
2025-05-10 11:22:39 +0300

    fix: skills async

--- frontend/src/components/TextSection/TextSection.tsx
+++ frontend/src/components/TextSection/TextSection.tsx
@@ -7,54 +7,10 @@ import { Trans, useTranslation } from 'react-i18next';
 
 import { Categories } from '@/config/categories';
 import { Technologies } from '@/config/technologies';
+import { ListEnumeration } from '@/components/TextSection/ListEnumeration';
 
 interface TextSectionProps {
   onLinkClick?: (tabIndex: number) => void;
-}
-
-interface ListEnumerationProps {
-  list: Category[];
-  maxToDisplay?: number;
-  translationKey: string;
-}
-
-function ListEnumeration({
-  list,
-  maxToDisplay = 3,
-  translationKey,
-}: ListEnumerationProps) {
-  const { t } = useTranslation();
-  if (!list?.length) return '';
-  const displayList = list.slice(0, maxToDisplay);
-  const renderItem = (item: Category) => {
-    return (
-      <span className="text-text">{t(`${translationKey}.${item.name}`)}</span>
-    );
-  };
-  if (displayList.length === 1) {
-    return renderItem(displayList[0]);
-  }
-  if (displayList.length === 2) {
-    return (
-      <>
-        {renderItem(displayList[0])} {t('common.and')}{' '}
-        {renderItem(displayList[1])}
-      </>
-    );
-  }
-  return (
-    <>
-      {displayList
-        .slice(0, -1)
-        .map((item) => renderItem(item))
-        .reduce((result, item) => (
-          <>
-            {result}, {item}
-          </>
-        ))}{' '}
-      {t('common.and')} {renderItem(displayList[displayList.length - 1])}
-    </>
-  );
 }
 
 export function TextSection({ onLinkClick }: TextSectionProps) {
