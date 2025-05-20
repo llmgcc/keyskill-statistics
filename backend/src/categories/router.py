3465bde0435b083918b022242e1dc5e79a772a5b
llmgcc <llmgcc8926@gmail.com>
2025-05-20 16:46:36 +0300

    fix: categories service

--- backend/src/categories/router.py
+++ backend/src/categories/router.py
@@ -1,15 +1,30 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session
 from src.dependencies import get_async_session
 from src.categories.service import categories_list
 from src.categories.schemas import CategoriesResponse
+from src.constants import Experience
 
 router = APIRouter(prefix="/categories", tags=["Categories"])
 
 
-@router.get(summary="Categories", path="/list", response_model=List[CategoriesResponse])
+@router.get(
+    path="/list",
+    summary="Categories list",
+    response_model=List[CategoriesResponse],
+)
 async def get_categories_list(
-    session: Session = Depends(get_async_session), experience=None, period: int = 30
+    session: Session = Depends(get_async_session),
+    experience: str | None = Query(
+        None, description="Experience", example=Experience.noExperience
+    ),
+    period: int = Query(30, ge=1, description="Period", example=30),
 ):
-    return await categories_list(session, experience=experience, days_period=period)
+    try:
+        return await categories_list(session, experience=experience, days_period=period)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error: {str(e)}",
+        )
