f0a0e7b351e745f3d7f3155de4a0bae7696bd875
llmgcc <llmgcc8926@gmail.com>
2025-05-22 06:27:13 +0300

    fix: build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/keyskill-statistics/',
+  base: process.env.NODE_ENV === 'production' ? '/keyskill-statistics/' : '/',
   server: {
     // host: true,
     proxy: {
