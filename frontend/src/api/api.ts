dc61a153b07a5893df7a2a7db1356a70ee7a29c9
llmgcc <llmgcc8926@gmail.com>
2025-05-22 07:49:51 +0300

    fix: build static folder

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -5,6 +5,12 @@ const isStatic = import.meta.env.VITE_STATIC_API === 'true';
 
 export const API = isStatic ? new StaticAPI() : new ServerAPI();
 
-export const baseURL = isStatic
-  ? '/static-api/static'
+
+function getBaseUrl() {
+  const prefix = process.env.NODE_ENV === 'production' ? '/keyskill-statistics' : ''
+  return isStatic
+  ? `${prefix}/static-api/static`
   : 'http://localhost:8000/static';
+}
+
+export const baseURL = getBaseUrl();
